3aa5d4b514e3f6f98dc04439988619b2bffb9f8b
nada-medhat229 <nda96642@gmail.com>
2023-02-15 16:54:24 +0200

    update

--- js/script.js
+++ js/script.js
@@ -138,5 +138,24 @@ $(document).ready(function () {
       viewerHeight: 400,
     });
   });
+  const minusButton = document.getElementById('minus');
+const plusButton = document.getElementById('plus');
+const inputField = document.getElementById('input');
+
+  minusButton.addEventListener('click', event => {
+    event.preventDefault();
+    const currentValue = Number(inputField.value) || 0;
+    inputField.value = currentValue - 1;
+  });
+  
+  plusButton.addEventListener('click', event => {
+    event.preventDefault();
+    const currentValue = Number(inputField.value) || 0;
+    inputField.value = currentValue + 1;
+  }); 
+
+
+
   AOS.init();
+
 });
